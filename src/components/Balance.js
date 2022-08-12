5a4135201a154f7ba026cc90ce2d4b6998c7c741
armares <armares2001@libero.it>
2022-08-12 11:54:04 +0200

    IncomeExpenses

---
+++ src/components/Balance.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Balance.module.css";
+
+const Balance = () => {
+  return (
+    <React.Fragment>
+        <h4>Your Balance</h4>
+        <h1 id='balance'>$0.00</h1>
+    </React.Fragment>
+  )
+}
+
+export default Balance
